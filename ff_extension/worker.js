9373ef649a2b7da9424eeb632863abdb6d740fa7
Anthony Chu <itendstonight750@gmail.com>
2020-04-27 21:17:29 -0400

    commented out a line

--- ff_extension/worker.js
+++ ff_extension/worker.js
@@ -2,7 +2,7 @@ onmessage = function(e) {
     importScripts('crawler.js');
     let doc = e.data;
     
-    let links = JSONSearchForLinks(doc);
+    //let links = JSONSearchForLinks(doc);
         
         
     this.postMessage(doc)
